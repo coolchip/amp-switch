ce258149e866f5602bea80ffb4abd6cb01d53ece
Sebastian Balling <sebastian.balling@jumo.net>
2014-06-24 16:39:24 +0200

    print out when stopping count down

--- printStatusChange.py
+++ printStatusChange.py
@@ -37,7 +37,9 @@ def do_main_program( console ):
             if power == 0:
                 print("Power ON")
                 power = 1
-            shutdownTimer = SHUTDOWN_TIME
+			if shutdownTimer != SHUTDOWN_TIME:
+				shutdownTimer = SHUTDOWN_TIME
+				print("(stop count down) Power still ON")
 
         time.sleep(1)
 
