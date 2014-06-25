eb1f0c83ff6c5006b8a5f418f40936fff9a3539b
Sebastian Balling <sebastian.balling@jumo.net>
2014-06-25 17:22:43 +0200

    clean up & change licence model

---
+++ audiosubject.py
@@ -0,0 +1,47 @@
+# This file is part of Amp Switch
+#
+# Copyright (C) 2014 Sebastian Balling <coolchip@gmx.de>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class Subject:
+    def __init__(self):
+        self._observers = []
+
+    def attach(self, observer):
+        if not observer in self._observers:
+            self._observers.append(observer)
+
+    def detach(self, observer):
+        try:
+            self._observers.remove(observer)
+        except ValueError:
+            pass
+
+    def notify(self, modifier=None):
+        for observer in self._observers:
+            if modifier != observer:
+                observer.update(self)
+
+class AudioSubject(Subject):
+    def __init__(self):
+        Subject.__init__(self)
+        self.data = 0
+
+    def setData(self, data):
+        self.data = data
+        self.notify()
+
+    def getData(self):
+        return self.data
