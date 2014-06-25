6bb253e7b06759ea7e55bcf3b3ef9dced00fe72f
Sebastian Balling <sebastian.balling@jumo.net>
2014-06-25 11:04:12 +0200

    renamed vars and export hwinit

--- amp-switch.py
+++ amp-switch.py
@@ -8,6 +8,12 @@ def isAudioPlaying():
 
 import RPi.GPIO as GPIO
 
+def initHardware():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.output(11, GPIO.LOW)
+
 def powerOn(console):
     GPIO.output(11, GPIO.HIGH)
     if console:
@@ -20,14 +26,10 @@ def powerOff(console):
 
 import time
 
-def main( console = False, shutdownTime = 200 ):
-    shutdownTimer = shutdownTime
+def main( console, powerOffDelay ):
+    shutdownTimer = powerOffDelay
     power = 0
-
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(11, GPIO.OUT)
-    GPIO.output(11, GPIO.LOW)
+    initHardware()
 
     loop = True
     while loop:
@@ -37,9 +39,9 @@ def main( console = False, shutdownTime = 200 ):
             if power == 0:
                 powerOn(console)
                 power = 1
-                shutdownTimer = shutdownTime
-            if shutdownTimer != shutdownTime:
-                shutdownTimer = shutdownTime
+                shutdownTimer = powerOffDelay
+            if shutdownTimer != powerOffDelay:
+                shutdownTimer = powerOffDelay
                 if console:
                     print('Stopping count down (Power is still ON)')
         else:
@@ -52,25 +54,21 @@ def main( console = False, shutdownTime = 200 ):
                     print("count down... " + str(shutdownTimer))
         time.sleep(0.1)
 
-
 import os
 from optparse import OptionParser
 import daemon
 
 if __name__ == "__main__":
     parser = OptionParser( os.path.relpath(__file__) + " [-s xxx] [-c]|[-d]" )
-
-    parser.add_option("-s", "--shutdowntime",  action="store", dest="shutdowntime", default=200, type="int", help="set the shutdown time (seconds)")
-    parser.add_option("-d", "--daemon", action="store_true", dest="daemon", default=False, help="start as daemon")
+    parser.add_option("-doff", "--poweroffdelay",  action="store", dest="poweroffdelay", default=200, type="int", help="set the power off delay time (seconds)")
+    parser.add_option("-d", "--daemon", action="store_true", dest="daemon", default=False, help="run as daemon")
     parser.add_option("-c", "--console", action="store_true", dest="console", default=False, help="output on console")
-
     (optionen, args) = parser.parse_args()
 
-    argShutdownTime = optionen.shutdowntime
     if optionen.daemon:
         with daemon.DaemonContext():
-            main(console=False, shutdownTime=argShutdownTime)
+            main(console=False, powerOffDelay=optionen.poweroffdelay)
     else:
-        main(console=optionen.console, shutdownTime=argShutdownTime)
+        main(console=optionen.console, powerOffDelay=optionen.poweroffdelay)
 
     sys.exit(0)
